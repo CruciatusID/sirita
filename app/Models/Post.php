faeffbb969701c3ea7feb86ba736d6a2cf8774f1
CruciatusID <andrewpangala@gmail.com>
2026-05-12 13:32:54 +0800

    Turunkan stack aplikasi ke PHP 8.2

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -8,8 +8,8 @@ use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
-use Spatie\Activitylog\Models\Concerns\LogsActivity;
-use Spatie\Activitylog\Support\LogOptions;
+use Spatie\Activitylog\LogOptions;
+use Spatie\Activitylog\Traits\LogsActivity;
 
 #[Fillable([
     'title',
